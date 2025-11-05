8db73b66d674360db3065e1a727c073899f1a242
Axeniter <anamievoid@gmail.com>
2025-11-05 23:27:09 +1000

    Add: Blur filter

---
+++ GraphicEditor/Models/BlurFilter.cs
@@ -0,0 +1,34 @@
+﻿using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphicEditor.Models
+{
+    internal class BlurFilter : IFilter
+    {
+        public float MinIntensity => 0f;
+
+        public float MaxIntensity => 20f;
+        public float Intensity { get; set; } = 1f;
+
+        public string Name => "Blur";
+
+        public SKBitmap ProcessImage(SKBitmap bitmap)
+        {
+            if (bitmap == null) return null;
+
+            var result = new SKBitmap(bitmap.Width, bitmap.Height, bitmap.ColorType, bitmap.AlphaType);
+
+            using var canvas = new SKCanvas(result);
+            using var paint = new SKPaint();
+
+            paint.ImageFilter = SKImageFilter.CreateBlur(Intensity, Intensity);
+            canvas.DrawBitmap(bitmap, 0, 0, paint);
+
+            return result;
+        }
+    }
+}
